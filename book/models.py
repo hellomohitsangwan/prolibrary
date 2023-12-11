7f17d03fcf6eecd68ee15f908a499809dac217d6
hellomohitsangwan <hellomohitsangwan@gmail.com>
2023-12-11 12:07:26 +0530

    book model

--- book/models.py
+++ book/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Book(models.Model):
+    title=models.CharField(max_length=50)
+    description=models.CharField()
+    category=models.CharField()
+    price=models.IntegerField()
+
+
+    def __str__(self):
+        return self.title
